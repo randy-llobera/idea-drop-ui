002bcf910513b8e8c3f3da3e7e3c6cc371fb3887
Randy Llobera <randy.llobera@gmail.com>
2026-02-11 14:45:16 +0100

    Add IdeaCard component. Refactor idea list on Ideas and Home pages

--- src/components/IdeaCard.tsx
+++ src/components/IdeaCard.tsx
@@ -1,5 +1,35 @@
-const IdeaCard = () => {
-  return <></>;
+import { Link } from '@tanstack/react-router';
+import type { Idea } from '@/types';
+import clsx from 'clsx';
+
+type IdeaCardProps = {
+  idea: Idea;
+  button?: boolean;
+};
+
+const IdeaCard = ({ idea, button = true }: IdeaCardProps) => {
+  const linkClasses = clsx({
+    'text-blue-600 hover:underline mt-3': !button,
+    'text-center mt-4 inline-block bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition':
+      button,
+  });
+
+  return (
+    <div className='border border-gray-300 p-4 rounded shadow bg-white flex flex-col justify-between'>
+      <div>
+        <h2 className=' text-lg font-semibold'>{idea.title}</h2>
+        <p className='text-gray-700 mt-2'>{idea.summary}</p>
+      </div>
+
+      <Link
+        to='/ideas/$ideaId'
+        params={{ ideaId: idea.id.toString() }}
+        className={linkClasses}
+      >
+        {button ? 'View Idea' : 'Read more →'}
+      </Link>
+    </div>
+  );
 };
 
 export default IdeaCard;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { createFileRoute, Link } from '@tanstack/react-router';
 import { queryOptions, useSuspenseQuery } from '@tanstack/react-query';
 import { Lightbulb } from 'lucide-react';
 import { fetchIdeas } from '@/api/ideas';
+import IdeaCard from '@/components/IdeaCard';
 
 const ideasQueryOptions = queryOptions({
   queryKey: ['ideas'],
@@ -34,24 +35,11 @@ function HomePage() {
         <h2 className='text-2xl font-semibold mb-4 text-gray-800'>
           Latest Ideas
         </h2>
-        <ul className='space-y-6'>
+        <div className='space-y-6'>
           {latestIdeas.map((idea) => (
-            <li
-              key={idea.id}
-              className='border border-gray-300 rounded-lg shadow p-4 bg-white'
-            >
-              <h3 className='text-lg font-bold text-gray-900'>{idea.title}</h3>
-              <p className='text-gray-600 mb-2'>{idea.summary}</p>
-              <Link
-                to='/ideas/$ideaId'
-                params={{ ideaId: idea.id.toString() }}
-                className='text-blue-600 hover:underline'
-              >
-                Read more →
-              </Link>
-            </li>
+            <IdeaCard key={idea.id} idea={idea} button={false} />
           ))}
-        </ul>
+        </div>
 
         <div className='mt-6'>
           <Link
