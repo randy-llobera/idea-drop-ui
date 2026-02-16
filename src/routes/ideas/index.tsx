abf9b0c1b3ccfeb8ede99c38fd56b9b2ac57a8bc
Randy Llobera <randy.llobera@gmail.com>
2026-02-16 16:23:34 +0100

    Switch to sorting and limit in the api rather than doing it in the frontend

--- src/routes/ideas/index.tsx
+++ src/routes/ideas/index.tsx
@@ -15,10 +15,8 @@ export const Route = createFileRoute('/ideas/')({
 });
 
 function IdeasPage() {
-  const { data } = useSuspenseQuery(ideasQueryOptions);
-  const ideas = [...data].sort(
-    (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
-  );
+  const { data: ideas } = useSuspenseQuery(ideasQueryOptions);
+
   return (
     <div className='p-4'>
       <h1 className='text-2xl font-bold mb-4'>Ideas</h1>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,8 +5,8 @@ import { fetchIdeas } from '@/api/ideas';
 import IdeaCard from '@/components/IdeaCard';
 
 const ideasQueryOptions = queryOptions({
-  queryKey: ['ideas'],
-  queryFn: fetchIdeas,
+  queryKey: ['ideas', { limit: 3 }],
+  queryFn: () => fetchIdeas(3),
 });
 
 export const Route = createFileRoute('/')({
@@ -17,12 +17,6 @@ export const Route = createFileRoute('/')({
 
 function HomePage() {
   const { data: ideas } = useSuspenseQuery(ideasQueryOptions);
-  const latestIdeas = [...ideas]
-    .sort(
-      (a, b) =>
-        new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
-    )
-    .slice(0, 3);
 
   return (
     <div className='flex flex-col md:flex-row items-start justify-between gap-10 p-6 text-blue-600'>
@@ -41,7 +35,7 @@ function HomePage() {
           Latest Ideas
         </h2>
         <div className='space-y-6'>
-          {latestIdeas.map((idea) => (
+          {ideas.map((idea) => (
             <IdeaCard key={idea._id} idea={idea} button={false} />
           ))}
         </div>
